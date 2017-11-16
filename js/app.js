1c1b43267f708d78b01f2c8388c47a3dd0511c00
simplonfl <fabrice-laroche81@live.fr>
2017-11-16 12:23:34 +0100

    Calculs

---
+++ js/app.js
@@ -0,0 +1,44 @@
+console.log('welcome to the real world');
+
+
+function add(a,b){
+
+	return a+b;
+}
+	console.log(add(10, 5));
+
+	
+
+console.log('The Matrix has you...');
+
+
+function sub(a,b){
+
+	return a-b;
+}
+	console.log(sub(10, 5));
+
+	
+
+console.log('Follow the white rabbit');
+
+
+function divi(a,b){
+ 
+	return a/b;
+}
+	console.log(divi(10, 5));
+
+	
+
+console.log('Do not try and bend the spoon. Thats impossible. Instead... only try to realize the truth.');
+
+
+function multi(a,b){
+ 
+	return a*b;
+}
+	console.log(multi(10, 5));
+
+
+
